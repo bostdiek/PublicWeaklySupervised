8d3bb22e0519240e57af2d6a6697a6fe47f4865c
bostdiek <bostdiek@gmail.com>
2017-06-30 09:38:59 -0700

    styled to pep8

--- Functions/SampleGenerators.py
+++ Functions/SampleGenerators.py
@@ -1,107 +1,137 @@
 from scipy.stats import norm
 import numpy as np
 
-# Function to make a data set which has percentage_A samples drawn from parameters_A
-# and 1-percentage_A drawn from parameters_B. The TotalSize is defaulted to 200000 but can be changed.
+# Function to make a data set which has percentage_A samples drawn from
+# parameters_A and 1-percentage_A drawn from parameters_B. The TotalSize
+# is defaulted to 200000 but can be changed.
+
+
 def MakeSamples(parameters_A, parameters_B, percentage_A, TotalSize=200000):
-    sizeA = int(percentage_A*TotalSize)
-    sizeB = TotalSize-sizeA
-    
-    setA=[]
-    for mu0, sigma0 in parameters_A:
-        setA.append(norm.rvs(loc=mu0,scale=sigma0,size=sizeA))
-    setA=np.array(setA)
-    ones = np.ones([1,setA.shape[1]])
-    setA=np.transpose(np.append(setA,ones,axis=0))
+    sizeA = int(percentage_A * TotalSize)
+    sizeB = TotalSize - sizeA
 
-    
-    setB=[]
+    setA = []
+    for mu0, sigma0 in parameters_A:
+        setA.append(norm.rvs(loc=mu0, scale=sigma0, size=sizeA))
+    setA = np.array(setA)
+    ones = np.ones([1, setA.shape[1]])
+    setA = np.transpose(np.append(setA, ones, axis=0))
+
+    setB = []
     for mu0, sigma0 in parameters_B:
-        setB.append(norm.rvs(loc=mu0,scale=sigma0,size=sizeB))
-    setB=np.array(setB)
-    zeros = np.zeros([1,setB.shape[1]])
-    setB=np.transpose(np.append(setB,zeros,axis=0))
-  
-    npout = np.vstack([setA,setB])
-    
-    npout = np.concatenate( (npout,percentage_A * np.ones([npout.shape[0],1])), axis=1)
-    
+        setB.append(norm.rvs(loc=mu0, scale=sigma0, size=sizeB))
+    setB = np.array(setB)
+    zeros = np.zeros([1, setB.shape[1]])
+    setB = np.transpose(np.append(setB, zeros, axis=0))
+
+    npout = np.vstack([setA, setB])
+
+    npout = np.concatenate((npout,
+                            percentage_A * np.ones([npout.shape[0], 1])
+                            ),
+                           axis=1)
+
     return npout
 
-# Function to make a data set which has percentage_A samples drawn from parameters_A
-# and 1-percentage_A drawn from parameters_B.
-# This function labels the data with percentage_A_Expected even though percentage_A is the real value used
-# in splitting the data.
-def MakeSamplesWithError(parameters_A, parameters_B, percentage_A, percentage_A_Expected, TotalSize=200000):
-   
-    sizeA = int(percentage_A*TotalSize)
-    sizeB = TotalSize-sizeA
-    
-    setA=[]
+# Function to make a data set which has percentage_A samples drawn from
+# parameters_A and 1-percentage_A drawn from parameters_B.
+# This function labels the data with percentage_A_Expected even
+# though percentage_A is the real value used in splitting the data.
+
+
+def MakeSamplesWithError(parameters_A,
+                         parameters_B,
+                         percentage_A,
+                         percentage_A_Expected,
+                         TotalSize=200000):
+
+    sizeA = int(percentage_A * TotalSize)
+    sizeB = TotalSize - sizeA
+
+    setA = []
     for mu0, sigma0 in parameters_A:
-        setA.append(norm.rvs(loc=mu0,scale=sigma0,size=sizeA))
-    setA=np.array(setA)
-    ones = np.ones([1,setA.shape[1]])
-    setA=np.transpose(np.append(setA,ones,axis=0))
-    
-    setB=[]
+        setA.append(norm.rvs(loc=mu0, scale=sigma0, size=sizeA))
+    setA = np.array(setA)
+    ones = np.ones([1, setA.shape[1]])
+    setA = np.transpose(np.append(setA, ones, axis=0))
+
+    setB = []
     for mu0, sigma0 in parameters_B:
-        setB.append(norm.rvs(loc=mu0,scale=sigma0,size=sizeB))
-    setB=np.array(setB)
-    zeros = np.zeros([1,setB.shape[1]])
-    setB=np.transpose(np.append(setB,zeros,axis=0))
-    
-    npout = np.vstack([setA,setB])    
-    npout = np.concatenate( (npout,percentage_A_Expected * np.ones([npout.shape[0],1])), axis=1)
-    
+        setB.append(norm.rvs(loc=mu0, scale=sigma0, size=sizeB))
+    setB = np.array(setB)
+    zeros = np.zeros([1, setB.shape[1]])
+    setB = np.transpose(np.append(setB, zeros, axis=0))
+
+    npout = np.vstack([setA, setB])
+    npout = np.concatenate((npout,
+                            percentage_A_Expected * np.ones([npout.shape[0], 1])
+                            ),
+                           axis=1)
     return npout
 
-# Function to make a data set which has percentage_A samples drawn from parameters_A
-# and 1-percentage_A drawn from parameters_B.
-# This function labels the data with percentage_A_Expected even though percentage_A is the real value used
-# in splitting the data.
+# Function to make a data set which has percentage_A samples drawn
+# from parameters_A and 1-percentage_A drawn from parameters_B.
+# This function labels the data with percentage_A_Expected even though
+# percentage_A is the real value used in splitting the data.
 # Now parameters_A and parameters_B can have multiple gaussians in them
-def MakeMultiGSamples(parameters_A, parameters_B, percentage_A, percentage_A_Expected, TotalSize=200000):
-   
-    sizeA = int(percentage_A*TotalSize)
-    sizeB = TotalSize-sizeA
+
+
+def MakeMultiGSamples(parameters_A,
+                      parameters_B,
+                      percentage_A,
+                      percentage_A_Expected,
+                      TotalSize=200000):
+
+    sizeA = int(percentage_A * TotalSize)
+    sizeB = TotalSize - sizeA
     print sizeA, sizeB
-    
-    setA=[]
+
+    setA = []
     for feature in parameters_A:
-        #print feature
-        tmp_feat=np.array([])
+        # print feature
+        tmp_feat = np.array([])
         for (mu0, sigma0), percent in feature:
-            #print mu0, sigma0, percent
-            tmp_feat=np.append(tmp_feat,norm.rvs(loc=mu0,scale=sigma0,size=int(sizeA*percent)))
-            #print "tmp_feat:",tmp_feat
+            # print mu0, sigma0, percent
+            tmp_feat = np.append(tmp_feat,
+                                 norm.rvs(loc=mu0,
+                                          scale=sigma0,
+                                          size=int(sizeA * percent)
+                                          )
+                                 )
+            # print "tmp_feat:",tmp_feat
             np.random.shuffle(tmp_feat)
         setA.append(tmp_feat)
-        #print setA
-                            
-    setA=np.array(setA)
-    ones = np.ones([1,setA.shape[1]])
-    setA=np.transpose(np.append(setA,ones,axis=0))
-    #print setA
-    
-    setB=[]
+        # print setA
+
+    setA = np.array(setA)
+    ones = np.ones([1, setA.shape[1]])
+    setA = np.transpose(np.append(setA, ones, axis=0))
+    # print setA
+
+    setB = []
     for feature in parameters_B:
-        tmp_feat=np.array([])
+        tmp_feat = np.array([])
         for (mu0, sigma0), percent in feature:
-            #print mu0, sigma0, percent
-            tmp_feat=np.append(tmp_feat,norm.rvs(loc=mu0,scale=sigma0,size=int(sizeB*percent)))
+            # print mu0, sigma0, percent
+            tmp_feat = np.append(tmp_feat,
+                                 norm.rvs(loc=mu0,
+                                          scale=sigma0,
+                                          size=int(sizeB * percent)
+                                          )
+                                 )
             np.random.shuffle(tmp_feat)
         setB.append(tmp_feat)
-    setB=np.array(setB)
-    zeros = np.zeros([1,setB.shape[1]])
-    setB=np.transpose(np.append(setB,zeros,axis=0))
-    
-    print "Set1 shape:",setA.shape
-    print "Set2 shape:",setB.shape
-    
-    
-    
-    npout = np.vstack([setA,setB])    
-    npout = np.concatenate( (npout,percentage_A_Expected * np.ones([npout.shape[0],1])), axis=1)
-    
+    setB = np.array(setB)
+    zeros = np.zeros([1, setB.shape[1]])
+    setB = np.transpose(np.append(setB, zeros, axis=0))
+
+    print "Set1 shape:", setA.shape
+    print "Set2 shape:", setB.shape
+
+    npout = np.vstack([setA, setB])
+    npout = np.concatenate((npout,
+                            percentage_A_Expected * np.ones([npout.shape[0], 1])
+                            ),
+                           axis=1)
+
     return npout
--- Functions/WeakCostFunction.py
+++ Functions/WeakCostFunction.py
@@ -8,22 +8,26 @@ REGULARIZATION = 1e-8
 #   percentage of the total data set which are in class 1
 # y_pred is the prediction for each point (in the range 0-1)
 # the output is the cost
+
+
 def WeakSupervision(y_true, y_pred):
-    return K.abs((K.mean(y_pred, axis=-1) - K.mean(y_true,axis=-1)))
+    return K.abs((K.mean(y_pred, axis=-1) - K.mean(y_true, axis=-1)))
 
 # Function takes in two Keras tensors
 # y_true is NOT the standard label for a classifier, but rather the
 #   percentage of the total data set which are in class 1
 # y_pred is the prediction for each point (in the range 0-1)
 # the output is the cost
+
+
 def WeakSupervision_v2(y_true, y_pred):
-    loss1 = K.mean(y_pred, axis=-1) - K.mean(y_true,axis=-1)
+    loss1 = K.mean(y_pred, axis=-1) - K.mean(y_true, axis=-1)
     constrib = REGULARIZATION * K.std(y_pred)
     loss1 = K.square(loss1) - constrib
-    
-    loss2 = (1.0 - K.mean(y_pred, axis=-1)) - K.mean(y_true,axis=-1)
+
+    loss2 = (1.0 - K.mean(y_pred, axis=-1)) - K.mean(y_true, axis=-1)
     loss2 = K.square(loss2) - constrib
-    
-    #loss = K.switch(K.less(loss1,loss2),loss1,loss2)
-    loss = K.minimum(loss1,loss2)
+
+    # loss = K.switch(K.less(loss1,loss2),loss1,loss2)
+    loss = K.minimum(loss1, loss2)
     return loss
